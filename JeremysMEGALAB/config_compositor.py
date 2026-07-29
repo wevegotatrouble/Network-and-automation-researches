2f62f2c41c336ea306d5fa987131ed520c0cc4c2
wevegotatrouble <wevegotta@tuta.io>
2026-07-29 13:07:51 +0400

    Update config_compositor.py
    I update the script so that you can choose what you can do with your configs for mega lab or another network topology

--- JeremysMEGALAB/config_compositor.py
+++ JeremysMEGALAB/config_compositor.py
@@ -1,36 +1,82 @@
 #!/usr/bin/python3
-import sys
 from pathlib import Path
+import time
 from jinja2 import Environment, FileSystemLoader
 import yaml
 
-"""What must my project do?
-I think it must have environment no mater where this script works.
-This script should create dirs for the files and check whether these files exist or not.
-I presume these should be directories, not files.
-these script should parse  yaml configs.
-I think it should scna jinja templates too
-Or not
-Probably this script should take data from yaml configs
-And go through jinja templates for these configs
-The main point of the script is to write configs in txt files   w"""
-
-jinja_exts = ["*.jinja", "*.j2"]
-yaml_exts = ["*.yaml", "*.yml"]
-templates_dir = Path("templates")
-yaml_properties_dir = Path("yaml_properties")
-configs_dir = Path("configs")
-dirs = [templates_dir, yaml_properties_dir, configs_dir]
-yamlFiles = []
-jinjaFiles = []
+jinja_exts = ["*.jinja", "*.j2"] # Variables for jinja extensions
+yaml_exts = ["*.yaml", "*.yml"] # Variables for yaml extensions
+templates_dir = Path("templates") # The directory for our jinja templates
+yaml_properties_dir = Path("yaml_properties") # The directory for our yaml properties for the futue configs
+configs_dir = Path("configs") # The directory where script'll make configs in .txt format
+dirs = [templates_dir, yaml_properties_dir, configs_dir] # List for dirs_checker
+yamlFiles = [] # This list will save all yaml files which we have in the folder
+jinjaFiles = [] # This list will save all jinja files which we have in the folder
 
 
+def main_menu():
+    headlines = """Hello! This script created for making configs for Jeremy's MEGALAB
+    If  you wanted to make the similiar configs for your own 3-tier topology you should know how
+    this script works.
+
+    We have 4 jinja templates in template folder are access_switches.j2, core_switches.j2, 
+    distributed_switches.j2, routers.j2
+
+    If you want to make configs for your own topology keep in your mind the following dependencies:
+        - All router configs must start with 'R' in yaml_properties
+        - All core switches configs must start with 'C' in yaml_properties
+        - All distributed switches configs must start with 'D' in yaml_properties
+        - All access switches configs must start with 'A' in yaml_properties
+
+    Choose the actions:
+    1) Run the full scripts
+    2) Check the directories in your folder where you run your script
+    3) Check jinja templates
+    4) Check yaml yaml properties 
+    5) Make configs with the existed properties 
+    6) Show the main menu again
+    7) Exit from the script
+        """
+    choice = input(headlines)
+    # I made the menu where you can choose what script must do with configs and check your templates and properties
+    match choice:
+        case '1':
+            dirs_checker()
+            jinja_files_checker()
+            yaml_files_checker()
+            config_creator()
+            countdown()
+            main_menu()
+        case '2':
+            dirs_checker()
+            countdown()
+            main_menu()
+        case '3':
+            jinja_files_checker()
+            countdown()
+            main_menu()
+        case '4':
+            yaml_files_checker()
+            countdown()
+            main_menu()
+        case '5':
+            config_creator()
+            countdown()
+            main_menu()
+        case '6':
+            main_menu()
+        case '7':
+            exit()
+        case _:
+            print("Make a choice!")
+            print("You will be bring to the main menu")
+            countdown()
+            main_menu()
 
 def dirs_checker():
 
-
-    # Create the directory
-
+    # The simple function which checks whether you have folders for our tasks or not. 
+    # In case if you don't have the folders, the script will make it instead of you
     for dir in dirs:
         try:
             dir.mkdir()
@@ -45,13 +91,14 @@ def dirs_checker():
 
 def yaml_files_checker():
 
-    p = Path.cwd()
-    if list(p.rglob("*.yaml")) or list(p.rglob("*.yml")):
+    # The functinon which check all yaml properties for the future configs
+    p = Path.cwd() # Make a current folder where you run the cript the folder where we will manipulate with the files
+    if list(p.rglob("*.yaml")) or list(p.rglob("*.yml")): # We check files in the current folder by the extansions
         for y in yaml_exts:
             yaml_files = list(p.rglob(y))
             if yaml_files:
                 for k in yaml_files:
-                    print(f"Your config is {k.name}")
+                    print(f"Your yaml file is {k.name}")
                     yamlFiles.append(k)
     else:
         print(
@@ -75,7 +122,6 @@ def jinja_files_checker():
         )
 
 def config_creator():
-     # Check if the path is absolute
     for a in yamlFiles:
         env = Environment(loader=FileSystemLoader("templates"),trim_blocks=True,lstrip_blocks=True)
         # Reading YAML data from file
@@ -84,86 +130,43 @@ def config_creator():
         with open(f"yaml_properties/{device_name}", "r") as f:
             yaml_data = yaml.load(f, Loader=yaml.FullLoader)
 
-        #print(yaml_data)
 
         if yaml_data is None:
-            print(f"Your config file {device_name} is empty")
+            print(f"Your yaml property for {device_stem} is empty")
 
         elif device_name.startswith("R"):
-            #env = Environment(loader=FileSystemLoader("templates"))
             template = env.get_template("routers.j2")
             with open(f"configs/{device_stem}.txt", "w") as f:
                 f.write(template.render(yaml_data))
+            print(f"Your config file for {device_stem} is ready")
 
         elif device_name.startswith("C"):
-            #env = Environment(loader=FileSystemLoader("templates"))
             template = env.get_template("core_switches.j2")
             with open(f"configs/{device_stem}.txt", "w") as f:
                 f.write(template.render(yaml_data))
+            print(f"Your config file for {device_stem} is ready")
 
         elif device_name.startswith("D"):
-            #env = Environment(loader=FileSystemLoader("templates"))
             template = env.get_template("distributed_switches.j2")
             with open(f"configs/{device_stem}.txt", "w") as f:
                 f.write(template.render(yaml_data))
+            print(f"Your config file for {device_stem} is ready")
 
         else:
-            #env = Environment(loader=FileSystemLoader("templates"))
             template = env.get_template("access_switches.j2")
             with open(f"configs/{device_stem}.txt", "w") as f:
                 f.write(template.render(yaml_data))
+            print(f"Your config file for {device_stem} is ready")
 
-def read_lists():
-    #print(yamlFiles)
-    #print(jinjaFiles)
-
-    # Check if the path is absolute
-    for a in yamlFiles:
-        #print("Is absolute:", a.is_absolute())
-
-        # Check if the path is absolute
-        print("File name:", a.name)
-
-        # Check if the path is absolute
-        #print("Extension:", a.suffix)
-
-        # Check if the path is absolute
-        #print("Parent Directory:", a.parent)
-    #for b in jinjaFiles:
-        #print("Is absolute:", b.is_absolute())
-
-        ## Check if the path is absolute
-        #print("File name:", b.name)
-
-        ## Check if the path is absolute
-        #print("Extension:", b.suffix)
-
-        ## Check if the path is absolute
-        #print("Parent Directory:", b.parent)
-
-def yaml_parser():
-    
-    # Check if the path is absolute
-    for a in yamlFiles:
-
-        # Check if the path is absolute
-        device_parameters = a.name
-        device_stem = a.stem
-        print("File name with extension:", device_parameters)
-        print("File name without extension:", device_stem)
-        #if device_parameters.startswith("R"):
-        #    print("File name with extension:", device_parameters)
-
-
-
+def countdown():
+    t = 5
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end='\r')  # Overwrite the line each second
+        time.sleep(1)
+        t -= 1
 
 if __name__ == "__main__":
-    dirs_checker()
-    jinja_files_checker()
-    yaml_files_checker()
-    #yaml_parser()
-    #read_lists()
-    config_creator() 
-
-
+    main_menu()
 
